9650c128e85f526083099b804494225fe15aa759
Wyatt Brown <wyatt.brown23@gmail.com>
2020-11-17 12:00:02 -0700

    working language, technology, and platform pages

--- portfolio.react/src/pages/TechnologyPage.jsx
+++ portfolio.react/src/pages/TechnologyPage.jsx
@@ -1,1 +1,58 @@
-﻿import * as React from "react";
+﻿import React from "react";
+import axios from "axios";
+import { Layout } from "antd";
+import "antd/dist/antd.css";
+import SideBar from "../components/SideBar";
+import PageHeader from "../components/PageHeader";
+
+const { Content } = Layout;
+
+
+class TechnologyPage extends React.Component {
+    state = {
+        technologies: [],
+    };
+
+    componentDidMount() {
+        axios
+            .get(`https://myportfolio-wyattb.herokuapp.com/api/category/GetTechnologiesWithProjects`)
+            .then((res) => {
+                const technologies = res.data;
+                this.setState({ technologies });
+                console.log(technologies);
+            });
+    }
+
+    render() {
+        const { technologies } = this.state;
+        return (
+            <Layout style={{ minHeight: "100vh" }}>
+                <PageHeader />
+                <Layout>
+                    <SideBar />
+                    <Content>
+                        <div>
+                            <h1>
+                                Technologies
+                            </h1>
+                        </div>
+                        <div>
+                            {Array.isArray(technologies) && technologies.map(object => (
+                                <li key={object.id}>
+                                    {object.name}
+                                    {object.projects.map(project => (
+                                        <ul key={project.id}>
+                                            {project.title}
+                                        </ul>
+                                    ))}
+                                </li>
+                            ))}
+                        </div>
+                    </Content>
+                </Layout>
+            </Layout>
+        );
+    }
+}
+
+export default TechnologyPage;
